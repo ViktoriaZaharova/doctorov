a7fb83abe011005b1efc848c692d4845e7f20187
Виктория Захарова <zaharova181114@gmail.com>
2024-02-25 23:15:17 +0200

    +1 page

--- js/common.js
+++ js/common.js
@@ -86,3 +86,31 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 	return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+// datepicker
+$(function () {
+	//Сменим язык календаря на русский
+	$.datepicker.setDefaults(
+		{
+			closeText: 'Закрыть',
+			prevText: '',
+			currentText: 'Сегодня',
+			monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+				'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+			monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
+				'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+			dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+			dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+			dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+			weekHeader: 'Не',
+			dateFormat: 'dd.mm.yy',
+			firstDay: 1,
+			isRTL: false,
+			showMonthAfterYear: false,
+			yearSuffix: ''
+		});
+	
+});
+$(function () {
+	$(".datepicker1").datepicker();
+});
