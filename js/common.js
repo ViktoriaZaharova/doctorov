0f8801b16764ba0ce3162f0bf73c22c61b36bfaf
Виктория Захарова <zaharova181114@gmail.com>
2024-03-01 00:54:51 +0200

    clinics mobile

--- js/common.js
+++ js/common.js
@@ -151,6 +151,14 @@ $('.reviews-slider').slick({
 	dots: true,
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	responsive: [
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+	]
 });
 
 $('.certificates-slider').slick({
