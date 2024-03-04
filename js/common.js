cc5882afd7c1b47ebcebf41707635362c29a0b1b
Виктория Захарова <zaharova181114@gmail.com>
2024-03-05 00:09:42 +0200

    edit2

--- js/common.js
+++ js/common.js
@@ -33,6 +33,59 @@ $('.go_to').click(function (e) {
 	return false;
 });
 
+// active menu navigation product
+$(document).ready(function () {
+	var sections = $(".section"),
+		nav = $(".nav-tabs-product"),
+		nav_height = nav.outerHeight();
+
+	$(window).on("scroll", function () {
+		var cur_pos = $(this).scrollTop();
+
+		sections.each(function () {
+			var top = $(this).offset().top - nav_height,
+				bottom = top + $(this).outerHeight();
+
+			if (cur_pos >= top && cur_pos <= bottom) {
+				nav.find("a").removeClass("active");
+				sections.removeClass("active");
+
+				$(this).addClass("active");
+				nav.find('a[href="#' + $(this).attr("id") + '"]').addClass("active");
+			}
+		});
+	});
+
+	nav.find("a").on("click", function () {
+		var $el = $(this),
+			id = $el.attr("href");
+
+		$("html, body").animate(
+			{
+				scrollTop: $(id).offset().top - nav_height
+			},
+			500
+		);
+		$(".nav-tabs-product").find('.nav-link').removeClass('active');
+		$(this).addClass('active');
+
+		return false;
+	});
+
+})
+
+// tabs my
+$(".js-tab-trigger").click(function () {
+	var id = $(this).attr('data-tab'),
+		content = $('.js-tab-content[data-tab="' + id + '"]');
+
+	$('.js-tab-trigger.active').removeClass('active'); // 1
+	$(this).addClass('active'); // 2
+
+	$('.js-tab-content.active').removeClass('active'); // 3
+	content.addClass('active'); // 4
+});
+
 // search header
 $('.btn-search').on('click', function () {
 	$('.header-search-wrapper').fadeToggle();
